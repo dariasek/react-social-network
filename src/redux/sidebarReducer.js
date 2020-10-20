2b17b3b3a1e8b911c8305ad2d313b20a900b6080
Руслан Сек <ruslansek@Ruslans-MacBook-Air.local>
2020-10-20 18:46:07 +0300

    back up

--- src/redux/sidebarReducer.js
+++
@@ -1,9 +0,0 @@
-let initialState = {
-    friends:['Sasha', 'Alex', 'Andrew'],
-}
-
-const sidebarReducer = (state = initialState, action) => {
-    return state
-}
-
-export default sidebarReducer;
